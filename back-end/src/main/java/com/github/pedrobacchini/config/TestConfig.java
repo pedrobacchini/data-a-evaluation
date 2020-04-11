1fdfb68d7ff473ae228cd559e2a4b9a6d7ce8e14
Pedro Bacchini <pedrobacchini@outlook.com>
2020-04-10 22:06:14 -0300

    Refactor TestConfig with appropriate CommandLineRunner class.

--- back-end/src/main/java/com/github/pedrobacchini/config/TestConfig.java
+++ back-end/src/main/java/com/github/pedrobacchini/config/TestConfig.java
@@ -8,72 +8,69 @@ import com.github.pedrobacchini.repository.ElectionRepository;
 import com.github.pedrobacchini.service.CandidateService;
 import com.github.pedrobacchini.service.ImageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Arrays;
 
 @Configuration
 @Profile("test")
-@RequiredArgsConstructor
 public class TestConfig {
 
-    private final ImageService imageService;
-    private final ElectionRepository electionRepository;
-    private final CandidateRepository candidateRepository;
-    private final CandidateService candidateService;
-
     @Bean
-    public boolean instantiateDatabase() throws IOException {
+    CommandLineRunner instantiateDatabase(final ImageService imageService,
+                                          final ElectionRepository electionRepository,
+                                          final CandidateRepository candidateRepository,
+                                          final CandidateService candidateService) {
 
-        ElectionPosition prefeito = new ElectionPosition("Prefeito");
-        ElectionPosition vereador = new ElectionPosition("Vereador");
+        return args -> {
+            ElectionPosition prefeito = new ElectionPosition("Prefeito");
+            ElectionPosition vereador = new ElectionPosition("Vereador");
 
-        ElectionPosition programadorLider = new ElectionPosition("Programador Lider");
-        ElectionPosition artistaLider = new ElectionPosition("Artista Lider");
+            ElectionPosition programadorLider = new ElectionPosition("Programador Lider");
+            ElectionPosition artistaLider = new ElectionPosition("Artista Lider");
 
-        ElectionPosition sindico = new ElectionPosition("Sindico");
-        ElectionPosition subsindico = new ElectionPosition("Sub-Sindico");
+            ElectionPosition sindico = new ElectionPosition("Sindico");
+            ElectionPosition subsindico = new ElectionPosition("Sub-Sindico");
 
-        Election eleicoes2020 = new Election("Eleições 2020",
-                LocalDate.of(2020, 10, 4), LocalDate.of(2020, 10, 25));
-        eleicoes2020.addAllElectionPositions(Arrays.asList(prefeito, vereador));
+            Election eleicoes2020 = new Election("Eleições 2020",
+                    LocalDate.of(2020, 10, 4), LocalDate.of(2020, 10, 25));
+            eleicoes2020.addAllElectionPositions(Arrays.asList(prefeito, vereador));
 
-        Election eleicoeslideres = new Election("Eleições Lideres",
-                LocalDate.of(2020, 3, 31), LocalDate.of(2020, 4, 20));
-        eleicoeslideres.addAllElectionPositions(Arrays.asList(programadorLider, artistaLider));
+            Election eleicoeslideres = new Election("Eleições Lideres",
+                    LocalDate.of(2020, 3, 31), LocalDate.of(2020, 4, 20));
+            eleicoeslideres.addAllElectionPositions(Arrays.asList(programadorLider, artistaLider));
 
-        Election eleicaoCodomino = new Election("Eleições Condominio 2019",
-                LocalDate.of(2019, 1, 12), LocalDate.of(2019, 3, 1));
-        eleicaoCodomino.addAllElectionPositions(Arrays.asList(sindico, subsindico));
+            Election eleicaoCodomino = new Election("Eleições Condominio 2019",
+                    LocalDate.of(2019, 1, 12), LocalDate.of(2019, 3, 1));
+            eleicaoCodomino.addAllElectionPositions(Arrays.asList(sindico, subsindico));
 
-        electionRepository.saveAll(Arrays.asList(eleicoes2020, eleicoeslideres, eleicaoCodomino));
+            electionRepository.saveAll(Arrays.asList(eleicoes2020, eleicoeslideres, eleicaoCodomino));
 
-        Candidate luiz = new Candidate("Luiz de souza", prefeito);
-        Candidate joao = new Candidate("João Ferreira", prefeito);
+            Candidate luiz = new Candidate("Luiz de souza", prefeito);
+            Candidate joao = new Candidate("João Ferreira", prefeito);
 
-        Candidate fernando = new Candidate("Fernado Paulo", vereador);
-        Candidate diego = new Candidate("Diego Paulo", vereador);
-        Candidate maria = new Candidate("Maria Dores", vereador);
+            Candidate fernando = new Candidate("Fernado Paulo", vereador);
+            Candidate diego = new Candidate("Diego Paulo", vereador);
+            Candidate maria = new Candidate("Maria Dores", vereador);
 
-        Candidate pedro = new Candidate("Pedro Bacchini", programadorLider);
-        Candidate henrique = new Candidate("Henrique Jose", programadorLider);
+            Candidate pedro = new Candidate("Pedro Bacchini", programadorLider);
+            Candidate henrique = new Candidate("Henrique Jose", programadorLider);
 
-        Candidate paulo = new Candidate("Paulo Cezar", artistaLider);
-        Candidate leonardo = new Candidate("Leonardo Medeiros", artistaLider);
+            Candidate paulo = new Candidate("Paulo Cezar", artistaLider);
+            Candidate leonardo = new Candidate("Leonardo Medeiros", artistaLider);
 
-        candidateRepository.saveAll(Arrays.asList(luiz, joao, fernando, diego, maria, pedro, henrique, paulo, leonardo));
+            candidateRepository.saveAll(Arrays.asList(luiz, joao, fernando, diego, maria, pedro, henrique, paulo, leonardo));
 
-        MultipartFile luizPicture = imageService.getMultipartFile("back-end/src/main/resources/static/01.jpg");
-        candidateService.uploadProfilePicture(luiz, luizPicture);
+            MultipartFile luizPicture = imageService.getMultipartFile("back-end/src/main/resources/static/01.jpg");
+            candidateService.uploadProfilePicture(luiz, luizPicture);
 
-        MultipartFile joaoPicture = imageService.getMultipartFile("back-end/src/main/resources/static/02.jpg");
-        candidateService.uploadProfilePicture(joao, joaoPicture);
-
-        return true;
+            MultipartFile joaoPicture = imageService.getMultipartFile("back-end/src/main/resources/static/02.jpg");
+            candidateService.uploadProfilePicture(joao, joaoPicture);
+        };
     }
 }
